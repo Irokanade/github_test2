890d4d80836251a32c9b73f1891d385f449c05ad
irokanade <michaeljs.leong@gmail.com>
2021-02-01 14:52:06 +0800

    Initial Commit

---
+++ github_test1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  github_test1
+//
+//  Created by Michael Leong on 2021/2/1.
+//
+
+#include <iostream>
+#include <cstdio>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
